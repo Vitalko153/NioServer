de9321d36038867674fe7d46266264ecde1cb672
DonVitalko <DonVitalko@mail.ru>
2021-06-28 19:20:16 +0300

    NioServer

--- src/main/java/NioTelnetServer.java
+++ src/main/java/NioTelnetServer.java
@@ -3,19 +3,17 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class NioTelnetServer {
     private static final String LS_COMMAND = "\tls     view all files from current directory";
     private static final String MKDIR_COMMAND = "\tmkdir  view all files from current directory";
+
+    Path pathServer = Path.of("server");     //Переменная с корневым путем.
 
     private final ByteBuffer buffer = ByteBuffer.allocate(512);
 
@@ -51,7 +49,7 @@ public class NioTelnetServer {
         System.out.println("Client connected. IP:" + channel.getRemoteAddress());
         channel.register(selector, SelectionKey.OP_READ, "skjghksdhg");
         channel.write(ByteBuffer.wrap("Hello user!\n".getBytes(StandardCharsets.UTF_8)));
-        channel.write(ByteBuffer.wrap("Enter --help for support info".getBytes(StandardCharsets.UTF_8)));
+        channel.write(ByteBuffer.wrap("Enter --help for support info.\n".getBytes(StandardCharsets.UTF_8)));
     }
 
 
@@ -86,17 +84,47 @@ public class NioTelnetServer {
         // добавить имя клиента
 
         if (key.isValid()) {
-            String command = sb.toString()
+            String[] cmd = sb.toString()
                     .replace("\n", "")
-                    .replace("\r", "");
-            if ("--help".equals(command)) {
+                    .replace("\r", "")
+                    .split(" ");
+            if ("--help".equals(cmd[0])) {
                 sendMessage(LS_COMMAND, selector, client);
                 sendMessage(MKDIR_COMMAND, selector, client);
-            } else if ("ls".equals(command)) {
+            } else if ("ls".equals(cmd[0])) {
                 sendMessage(getFilesList().concat("\n"), selector, client);
+
+                //создание файла
+            } else if ("touch".equals(cmd[0])) {
+                sendMessage(createFile(cmd[1],pathServer), selector, client);
+
+                //создание директории
+            } else if ("mkdir".equals(cmd[0])) {
+                sendMessage(createDirectory(cmd[1],pathServer), selector, client);
+
+                //удаление
+            }else if ("rm".equals(cmd[0])) {
+                sendMessage(remove(cmd[1],pathServer), selector, client);
+
+                //копирование
+            }else if ("copy".equals(cmd[0])) {
+                sendMessage(copy(cmd[1], pathServer, cmd[2]), selector, client);
+
+                //чтение из файла
+            } else if ("cat".equals(cmd[0])){
+                sendMessage(readFile(cmd[1], pathServer), selector, client);
+
+                //смена директории
+            } else if ("cd".equals(cmd[0])) {
+                sendMessage(changeDirectory(cmd[1]), selector, client);
+
+                //смена имени пользователя
+            } else if ("changenick".equals(cmd[0])){
+                sendMessage(changeNick(cmd[1], client), selector, client);
             }
         }
     }
+
 
     private void sendMessage(String message, Selector selector, SocketAddress client) throws IOException {
         for (SelectionKey key : selector.keys()) {
@@ -112,6 +140,63 @@ public class NioTelnetServer {
         String[] servers = new File("server").list();
         return String.join(" ", servers);
     }
+
+    //Создание файла
+    private String createFile(String fileName, Path pathServer) throws IOException {
+        Files.createFile(Path.of(String.valueOf(pathServer), fileName)) ;
+        return "File " + fileName + " created.\n";
+    }
+
+    //Создание директории
+    private String createDirectory(String dir, Path pathServer) throws IOException {
+        Files.createDirectory(Path.of(String.valueOf(pathServer), dir));
+        return "Directory " + dir + " created.\n";
+    }
+
+    //Удаление. Сделал перемещение в папку garbage.
+    private String remove(String fileName, Path pathServer) throws IOException {
+        Path garbage = Path.of("garbage"); //путь для корзины
+        Files.move(Path.of(String.valueOf(pathServer), fileName), Path.of(String.valueOf(garbage), fileName));
+        //После перемещение сделаю либо таймер либо по заполности удалять.
+        Files.delete(Path.of(String.valueOf(garbage), fileName));
+        //Не успел сделать рекурсивное удаление папки
+        return "File " + fileName + " moved in garbage and deleted.\n";
+    }
+
+    //Копирование
+    private String copy(String fileName, Path pathServer, String target) throws IOException {
+        Files.copy((Path.of(String.valueOf(pathServer), fileName)), (Path.of(String.valueOf(pathServer), target)));
+        return "File/Directory " + fileName + " copy in " + target;
+    }
+
+    //Чтение из файла
+    private String readFile(String fileName, Path pathServer) throws IOException {
+        List<String> textFile = Files.readAllLines(Path.of(String.valueOf(pathServer), fileName), StandardCharsets.UTF_8);
+        for (String s: textFile) {
+            return s;
+        }
+        return "\n";
+    }
+
+    //смена дирректории
+    private String changeDirectory(String cd) {
+        if ("~".equals(cd)){
+            pathServer = Path.of("server");
+        } else if ("..".equals(cd)){
+            pathServer = pathServer.getParent();
+        } else{
+            pathServer = Path.of(String.valueOf(pathServer), cd);
+        }
+        return "change directory " + pathServer;
+    }
+
+    //смена пользователя
+    private String changeNick(String name, SocketAddress client) {
+        clients.putIfAbsent(client, name);
+        return "New user " + name;
+
+    }
+
 
     public static void main(String[] args) throws Exception {
         new NioTelnetServer();
